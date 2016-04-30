3069910e41520d0fe0fcb880fba5bdd550625755
Olle Renius <ollre935@student.liu.se>
2016-04-30 08:12:38 +0200

    Remove an unused import from Hitbox.cpp

--- src/mapobjects/hitbox/Hitbox.cpp
+++ src/mapobjects/hitbox/Hitbox.cpp
@@ -1,8 +1,6 @@
 #include "Hitbox.h"
 #include "MathFunctions.h"
 
-#include <iostream>
-
 Hitbox::Hitbox(double x, double y, double xRel, double yRel, int radius, HitboxType hitboxType) :
     _x(x), _y(y), _xRel(xRel), _yRel(yRel), _radius(radius), _hitboxType(hitboxType) {
     // Calculate the distance from the center of the hitbox to the center of the MapObject

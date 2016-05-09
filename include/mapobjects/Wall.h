f9e95a642b5498743dd4d55a10e6953868d2af9d
Olle Renius <ollre935@student.liu.se>
2016-05-09 15:06:38 +0200

    Add an implementation of a wall object to the game

---
+++ include/mapobjects/Wall.h
@@ -0,0 +1,46 @@
+#ifndef WALL_H
+#define WALL_H
+
+#include "GraphicsManager.h"
+#include "MapObject.h"
+
+/**
+ * A MapObject representing a wall in the game.
+ * Moving MapObjects will simply bounce off the wall when colliding with it.
+ */
+class Wall : public MapObject {
+    public:
+
+        /**
+         * Constructor. Creates a wall object on the specified position.
+         * @param x - The x position where the object will be created
+         * @param y - The y position where the object will be created
+         * @param sprite - A pointer to a sprite to be used by the object
+         */
+        Wall(int x, int y, Sprite* sprite);
+
+        /**
+         * Constructor. Creates a wall object on the specified position.
+         * @param x - The x position where the object will be created
+         * @param y - The y position where the object will be created
+         * @param graphicsManager - the game's GraphicsManager object.
+         * This is used in the constructor to get the pre-defined sprite
+         * for the wall object.
+         */
+        Wall(int x, int y, GraphicsManager* graphicsManager);
+
+        /**
+         * Destructor.
+         */
+        ~Wall();
+
+    private:
+
+        /**
+         * Implementation of pure virtual method from the base class.
+         * Generates the hitboxes that are associated with the MapObject.
+         */
+        void generateHitboxes();
+};
+
+#endif // WALL_H
---
+++ src/mapobjects/Wall.cpp
@@ -0,0 +1,22 @@
+#include "Wall.h"
+#include "WallHitbox.h"
+
+Wall::Wall(int x, int y, Sprite* sprite) :
+    MapObject(x, y, sprite) {
+        generateHitboxes();
+}
+
+Wall::Wall(int x, int y, GraphicsManager* graphicsManager) :
+    MapObject(x, y, graphicsManager->getSprite(SpriteEnum::GREEN_SQUARE)) {
+        generateHitboxes();
+}
+
+Wall::~Wall() {
+}
+
+void Wall::generateHitboxes() {
+    _hitboxes.push_back(new WallHitbox(_x, _y, -16, -16, 8));
+    _hitboxes.push_back(new WallHitbox(_x, _y, -0, -16, 8));
+    _hitboxes.push_back(new WallHitbox(_x, _y, -0, -0, 8));
+    _hitboxes.push_back(new WallHitbox(_x, _y, -16, -0, 8));
+}
